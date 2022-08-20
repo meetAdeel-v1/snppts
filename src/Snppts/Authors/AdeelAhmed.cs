a2307493e8aca36f573910ec5c251237f10fa6ac
Adeel Ahmed <50557237+meetAdeel-v1@users.noreply.github.com>
2022-08-20 18:07:33 +0400

    Author added.

---
+++ src/Snppts/Authors/AdeelAhmed.cs
@@ -0,0 +1,12 @@
+namespace Snppts.Authors;
+
+public class AdeelAhmed : IAmAnAuthor
+{
+    public string FirstName => "Adeel";
+    public string LastName => "Ahmed";
+    public Uri Website => new Uri("");
+    public string TwitterHandle => "KyaYarAdeel";
+    public string GitHubHandle => "meetAdeel-v1";
+    public string EmailAddress => "ahmedadeel390@gmail.com";
+    public string GravatarHash => "0d3c5f787c3eb688ead3de1e48a85a1f";
+}
